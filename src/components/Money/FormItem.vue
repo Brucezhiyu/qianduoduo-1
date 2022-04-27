ac65523c64fb55537cd75f07d22ff2ee475acc9f
luzhiyu <lushuke5@gmail.com>
2022-04-27 21:21:12 +0800

    修复了value被赋值的问题

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,10 @@
   <div>
     <label class="formItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder">
+      <input type="text"
+             :value="value"
+             @input="onValueChanged($event.target.value)"
+             :placeholder="this.placeholder">
     </label>
   </div>
 </template>
@@ -13,7 +16,7 @@ import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component//装饰器
 export default class FormItem extends Vue {
-  value = '';
+  @Prop({default: ''})readonly value!: string;
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;
 
