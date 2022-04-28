e20945808d865bd79d355a0dda21e7d1a421f3c5
luzhiyu <lushuke5@gmail.com>
2022-04-28 17:39:49 +0800

    封装record的增操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,19 +15,17 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Types from '@/components/Money/Types.vue';
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import {Component} from 'vue-property-decorator';
+
 
 window.localStorage.setItem('version', '0.0.1');
-const recordList = recordListModel.fetch();
 
 @Component(
     {components: {Types, Tags, FormItem, NumberPad}}
 )
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList=window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -47,14 +45,8 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record)
+    window.createRecord(this.record)
   }
-
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
-
 }
 
 </script>
