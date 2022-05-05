9da10a0735335f9f1d4d3f94510d57009bc4282e
luzhiyu <lushuke5@gmail.com>
2022-05-05 23:08:20 +0800

    Labels.vue使用VueX，抽离Taghelper作为mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,7 +6,7 @@
       </li>
     </ul>
     <div class="new">
-      <button @click="create">
+      <button @click="createTag">
         新增标签
       </button>
     </div>
@@ -16,6 +16,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import {TagHelper} from '@/mixins/TagHelper';
 
 @Component({
   computed: {
@@ -24,11 +26,10 @@ import {Component, Prop} from 'vue-property-decorator';
     }
   }
 })//装饰器
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
 
   created() {
-
     this.$store.commit('fetchTags');
   }
 
@@ -40,16 +41,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit('update:value', this.selectedTags);
-  }
-
-  create() {
-    // const names = this.tagList.map(item => item.name);
-    //
-    const name = window.prompt('请输入标签名');
-    if (!name) {
-      window.alert('标签名不能为空');
-    }
-    this.$store.commit('createTag', name);
   }
 }
 </script>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,20 +18,28 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import Button from '@/components/Money/Button.vue';
-
+import {mixins} from 'vue-class-component';
+import {TagHelper} from '@/mixins/TagHelper';
 
 @Component({
-  components:{Button}
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    }
+  }
 })
-export default class Labels extends Vue {
-  tags = []//store.tagList//TODO
+export default class Labels extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit('fetchTags');
+  }
 
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (name) {
-      //TODO
-      //store.createTag(name)
+    if (!name) {
+      window.alert('标签名不能为空');
     }
+    this.$store.commit('createTag', name);
   }
 }
 </script>
