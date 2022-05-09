d436dca9985e9f3b81d43f428f3786e1d492ab3e
luzhiyu <lushuke5@gmail.com>
2022-05-09 22:36:05 +0800

    还有微信页面添加标签的bug未修复

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,8 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        createRecordError:null,
+        createTagError: null,
+        createRecordError: null,
         recordList: [],
         tagList: [],
         currentTag: undefined,
@@ -66,20 +67,21 @@ const store = new Vuex.Store({
         fetchTags(state) {
             state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]') as [];
             if (!state.tagList || state.tagList.length === 0) {
-                store.commit('createTag','衣')
-                store.commit('createTag','食')
-                store.commit('createTag','住')
-                store.commit('createTag','行')
+                store.commit('createTag', '衣');
+                store.commit('createTag', '食');
+                store.commit('createTag', '住');
+                store.commit('createTag', '行');
             }
         },
         createTag(state, name: string) {
+            state.createTagError = null;
             const names = state.tagList.map(item => item.name);
             if (names.indexOf(name) >= 0) {
-                window.alert('标签名重复');
+                state.createTagError = new Error('tag name duplicated');
                 return;
             }
             const id = createId().toString();
-            state.tagList.push({id: id, name: name});
+            state.tagList.push({id, name: name});
             store.commit('saveTags');
         },
         saveTags(state) {
